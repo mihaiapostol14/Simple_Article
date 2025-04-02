b36be7203594939d54110a5c7c20ed0c66474531
Mihai Apostol <mihaiapostol153@gmail.com>
2025-04-02 15:01:44 +0300

    added two new urls article-list and article-update

--- WebSite/article/urls.py
+++ WebSite/article/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('create-article/', views.ArticleFormView.as_view(), name='create-article'),
+    path('article-list/', views.ArticleListView.as_view(), name='article-list'),
+    path('article-update/<int:pk>/', views.ArticleUpdateView.as_view(), name='article-update'),
 ]
