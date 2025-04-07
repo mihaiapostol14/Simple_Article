e81764bc71dd91ac8b9e65ebd5c90f147f992b7a
Mihai Apostol <mihaiapostol153@gmail.com>
2025-04-07 15:35:19 +0300

    added two new url article-delete

--- WebSite/article/urls.py
+++ WebSite/article/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('create-article/', views.ArticleFormView.as_view(), name='create-article'),
     path('article-list/', views.ArticleListView.as_view(), name='article-list'),
     path('article-update/<int:pk>/', views.ArticleUpdateView.as_view(), name='article-update'),
+    path('article-delete/<int:pk>/', views.ArticleDeleteView.as_view(), name='article-delete'),
 ]
