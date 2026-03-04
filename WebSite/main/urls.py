61cf75fe29ef4bfabe35042b867f330aff5decc4
Mihai Apostol <mihaiapostol153@gmail.com>
2026-03-04 13:09:24 +0200

    added main app

---
+++ WebSite/main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
+    path('<int:year>/<str:month>', views.HomeView.as_view(), name='home'),
+
+]
